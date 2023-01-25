60513c5abadf764f0ab241aed56976f7099236d9
Maria S <mariasantiago0109@gmail.com>
2023-01-25 11:41:53 -0500

    answers for lesson 11

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-// extra packages
+// app.get('/', (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// });
+app.use(express.static('./public'))
 
 // routes
 app.use('/api/v1/auth', authRouter)
